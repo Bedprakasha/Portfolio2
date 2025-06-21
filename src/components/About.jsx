ede1e50ee425f1a66908f2552541544e50edf818
Bedprakasha <rtpqg000@gmail.com>
2025-06-21 23:51:22 +0545

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Bed, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+        Learning JavaScript , React, Node.js,Express.js  [2024-current]<br />
+        Bachelor in Computer Engineering at Cosmos College,Lalitpur  [2020-2025]<br />
+        +2 Science, Hari Khetan Multiple Campus, Birgunj [2018-2020]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [JavaScript,C, C++] Experienced with [React, Node.js, Git, MongoDB] Strong grasp of [Object-Oriented Programming (OOP), Responsive Design]
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        
+
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative Web Development solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
